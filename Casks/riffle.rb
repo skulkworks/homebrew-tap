6f61eb85814faeece9573419d5132d34a8dba3be
Andy Miller <rhuk@mac.com>
2026-08-05 22:11:20 +0100

    riffle 1.6.3 (42)

--- Casks/riffle.rb
+++ Casks/riffle.rb
@@ -1,6 +1,6 @@
 cask "riffle" do
-  version "1.6.2,41" # marketing,build — build-stamped DMG URL below
-  sha256 "890153391144ba53bf4c2e3c1f1e718d23dd0ec24e1a3225c06ef3036334e8ad"
+  version "1.6.3,42" # marketing,build — build-stamped DMG URL below
+  sha256 "10bcc93cb0ec9629a09589ea07110debbe370a5c0bfa3f8635595a86c2d4893a"
 
   url "https://updates.skulkworks.dev/riffle/Riffle-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Riffle"
